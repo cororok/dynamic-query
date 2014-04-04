c630b6300f2b81a2d5503f96a1b7c7f1a8551055
Songduk Park <cororok@gmail.com>
2014-04-03 20:39:02 -0400

    converted anonymouse clases to static classes

--- src/cororok/dq/util/MetaInfoMap.java
+++ src/cororok/dq/util/MetaInfoMap.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 
 /**
  * @author songduk.park cororok@gmail.com
- *
+ * 
  */
 public class MetaInfoMap {
 
@@ -18,67 +18,67 @@ public class MetaInfoMap {
 	static Map<ReadOnlyArray<String>, ReadOnlyArray<String>> COLUMN_MAP = new TreeMap<ReadOnlyArray<String>, ReadOnlyArray<String>>();
 	static Map<ReadOnlyInts, ReadOnlyInts> TYPE_MAP = new TreeMap<ReadOnlyInts, ReadOnlyInts>();
 
-	static SafeReadWrite<Class<?>, ReflectInfo> rwReflectionInfo;
-	static SafeReadWrite<ReadOnlyArray<String>, ReadOnlyArray<String>> rwColumnInfo;
-	static SafeReadWrite<ReadOnlyInts, ReadOnlyInts> rwTypeInfo;
+	static SafeReadWrite<Class<?>, ReflectInfo> rwReflectionInfo = new MetaInfoMap.SafeReadWriteReflection();
+	static SafeReadWrite<ReadOnlyArray<String>, ReadOnlyArray<String>> rwColumnInfo = new MetaInfoMap.SafeReadWriteColumn();
+	static SafeReadWrite<ReadOnlyInts, ReadOnlyInts> rwTypeInfo = new MetaInfoMap.SafeReadWriteType();
 
-	static {
-		rwReflectionInfo = new SafeReadWrite<Class<?>, ReflectInfo>() {
-			@Override
-			ReflectInfo writeLogic(Class<?> cl) {
-				// double check
-				ReflectInfo oldOne = readLogic(cl);
-				if (oldOne != null)
-					return oldOne;
+	static class SafeReadWriteReflection extends
+			SafeReadWrite<Class<?>, ReflectInfo> {
+		@Override
+		ReflectInfo writeLogic(Class<?> cl) {
+			// double check
+			ReflectInfo oldOne = readLogic(cl);
+			if (oldOne != null)
+				return oldOne;
 
-				ReflectInfo newOne = ReflectUtil.buildReflectInfo(cl);
-				REFLECT_INFO_MAP.put(cl.getName(), newOne);
-				return newOne;
-			}
+			ReflectInfo newOne = ReflectUtil.buildReflectInfo(cl);
+			REFLECT_INFO_MAP.put(cl.getName(), newOne);
+			return newOne;
+		}
 
-			@Override
-			ReflectInfo readLogic(Class<?> cl) {
-				return REFLECT_INFO_MAP.get(cl.getName());
-			}
-		};
+		@Override
+		ReflectInfo readLogic(Class<?> cl) {
+			return REFLECT_INFO_MAP.get(cl.getName());
+		}
+	};
 
-		rwColumnInfo = new SafeReadWrite<ReadOnlyArray<String>, ReadOnlyArray<String>>() {
-			@Override
-			ReadOnlyArray<String> writeLogic(ReadOnlyArray<String> newColumn) {
-				// double check
-				ReadOnlyArray<String> oldOne = readLogic(newColumn);
-				if (oldOne != null)
-					return oldOne;
+	static class SafeReadWriteColumn extends
+			SafeReadWrite<ReadOnlyArray<String>, ReadOnlyArray<String>> {
+		@Override
+		ReadOnlyArray<String> writeLogic(ReadOnlyArray<String> newColumn) {
+			// double check
+			ReadOnlyArray<String> oldOne = readLogic(newColumn);
+			if (oldOne != null)
+				return oldOne;
 
-				COLUMN_MAP.put(newColumn, newColumn);
-				return newColumn;
-			}
+			COLUMN_MAP.put(newColumn, newColumn);
+			return newColumn;
+		}
 
-			@Override
-			ReadOnlyArray<String> readLogic(ReadOnlyArray<String> newColumn) {
-				return COLUMN_MAP.get(newColumn);
-			}
-		};
+		@Override
+		ReadOnlyArray<String> readLogic(ReadOnlyArray<String> newColumn) {
+			return COLUMN_MAP.get(newColumn);
+		}
+	};
 
-		rwTypeInfo = new SafeReadWrite<ReadOnlyInts, ReadOnlyInts>() {
-			@Override
-			ReadOnlyInts writeLogic(ReadOnlyInts newTypes) {
-				// double check
-				ReadOnlyInts oldOne = readLogic(newTypes);
-				if (oldOne != null)
-					return oldOne;
+	static class SafeReadWriteType extends
+			SafeReadWrite<ReadOnlyInts, ReadOnlyInts> {
+		@Override
+		ReadOnlyInts writeLogic(ReadOnlyInts newTypes) {
+			// double check
+			ReadOnlyInts oldOne = readLogic(newTypes);
+			if (oldOne != null)
+				return oldOne;
 
-				TYPE_MAP.put(newTypes, newTypes);
-				return newTypes;
-			}
+			TYPE_MAP.put(newTypes, newTypes);
+			return newTypes;
+		}
 
-			@Override
-			ReadOnlyInts readLogic(ReadOnlyInts newTypes) {
-				return TYPE_MAP.get(newTypes);
-			}
-		};
-
-	}
+		@Override
+		ReadOnlyInts readLogic(ReadOnlyInts newTypes) {
+			return TYPE_MAP.get(newTypes);
+		}
+	};
 
 	/**
 	 * if old one exists return old one or create and put new one and return new
